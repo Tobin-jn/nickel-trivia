389f61a6ae750bab2d2155541948b99bbe969ae8
Tobin Nelson <tobin.j.n@gmail.com>
2018-11-01 15:28:36 -0600

    Setup Categories, DailyQuestions, TriviaGame container

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { NavLink } from 'react-router-dom';
 import './App.css';
+import Categories from '../containers/Categories';
+import DailyQuestion from '../containers/DailyQuestion';
+import TriviaGame from '../containers/TriviaGame';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1 className="title">Nickle Trivia</h1>
+        <button className="daily-trivia-btn">Trivia of the Day</button>
+        <button className="trivia-game">Trivia Game</button>
+        <Categories />
+        <DailyQuestion />
+        <TriviaGame />
+
       </div>
     );
   }
 }
 
 export default App;
+
+        // <NavLink className='trivia-of-day-btn' to='/dailycategory'>
+        //   Daily Trivia Question
+        // </NavLink>
+        // <NavLink className='question-generator-btn' to='/categories'>
+        //   Trivia Question Generator
+          
+        // </NavLink>
+
+// <NavLink to='/about'>About</NavLink>
