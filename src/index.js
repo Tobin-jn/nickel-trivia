aa8104831e02243bdf9c831448497224a528de02
Tobin Nelson <tobin.j.n@gmail.com>
2018-11-01 17:14:29 -0600

    Create store in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App/App';
+import React from 'react';
+import { BrowserRouter } from "react-router-dom";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "../src/reducers";
 import * as serviceWorker from './serviceWorker';
+import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(rootReducer, devTools);
+
+render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>, 
+  document.getElementById('root')
+);
 
 
 serviceWorker.unregister();
