0dfb640db1c9bd95b94d23a21951da0980fe2e0f
Tobin Nelson <tobin.j.n@gmail.com>
2018-11-02 10:25:59 -0600

    Create Home component

---
+++ src/components/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return(
+    <div>
+      <h1 className="title">Nickle Trivia</h1>
+      <NavLink className='daily-trivia-btn' to='/categories'>Trivia of the Day</NavLink>
+      <NavLink className='trivia-game-btn' to='/categories'>Trivia Game</NavLink>
+    </div>
+  )
+}
+
+export default Home
