a1b1ffde89568d42cfe97fd564a71165fe0c8ff6
Tobin Nelson <tobin.j.n@gmail.com>
2018-11-02 12:29:24 -0600

    Render questions with answer choices

--- src/containers/DailyQuestion/index.js
+++ src/containers/DailyQuestion/index.js
@@ -7,9 +7,15 @@ export class DailyQuestion extends Component {
 
 
   render() {
+    const { currentQuestion } = this.props
     return (
       <div>
         <h1>DailyQuestion</h1>
+        <h3 className="question">Question: {currentQuestion.question} </h3>
+        <p className="answer-choice choice-a">{currentQuestion.answers[0]}</p>
+        <p className="answer-choice choice-b">{currentQuestion.answers[1]}</p>
+        <p className="answer-choice choice-c">{currentQuestion.answers[2]}</p>
+        <p className="answer-choice choice-d">{currentQuestion.answers[3]}</p>
       </div>
     );
   }
@@ -18,7 +24,8 @@ export class DailyQuestion extends Component {
 
 export const mapStateToProps = state => ({
   category: state.category,
-  questions: state.questions
+  questions: state.questions,
+  currentQuestion: state.currentQuestion
 });
 
 export const mapDispatchToProps = dispatch => ({
