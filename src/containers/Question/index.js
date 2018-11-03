441ef844789ed3eb4a55d2a494ad7bbf16accf08
Tobin Nelson <tobin.j.n@gmail.com>
2018-11-03 09:34:37 -0600

    Render question container

--- src/containers/Question/index.js
+++ src/containers/Question/index.js
@@ -1,29 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Question.css';
 import PropTypes from "prop-types";
+import { connect } from 'react-redux'
 
 
-export const Question = props => {
-  return (
-    <div className="question-container">
-      <h3 className="question">Question: {currentQuestion.question} </h3>
-      <p className="answer-choice choice-a">{currentQuestion.answers[0]}</p>
-      <p className="answer-choice choice-b">{currentQuestion.answers[1]}</p>
-      <p className="answer-choice choice-c">{currentQuestion.answers[2]}</p>
-      <p className="answer-choice choice-d">{currentQuestion.answers[3]}</p>
-    </div>
-  );
+
+export class Question extends Component {
+
+  render() {
+    const { currentQuestion } = this.props
+    return (
+      <div className='question-container'>
+        <h3 className="question">Question: {currentQuestion.question} </h3>
+        <p className="answer-choice choice-a">{currentQuestion.answers[0]}</p>
+        <p className="answer-choice choice-b">{currentQuestion.answers[1]}</p>
+        <p className="answer-choice choice-c">{currentQuestion.answers[2]}</p>
+        <p className="answer-choice choice-d">{currentQuestion.answers[3]}</p>
+      </div>
+    );
+  }
+
 }
 
-
 export const mapStateToProps = state => ({
-  currentQuestion: state.cuurentQuestion,
-  category: state.category
+  category: state.category,
+  // questions: state.questions,
+  currentQuestion: state.currentQuestion
 });
 
 export const mapDispatchToProps = dispatch => ({
+  // saveUserData: (username, id) => dispatch(saveUserData(username, id)),
 
 });
+
 
 export default connect(
   mapStateToProps,
--- src/reducers/questionReducer.js
+++ src/reducers/questionReducer.js
@@ -1,4 +1,8 @@
-export const questionReducer = (state = {}, action) => {
+export const questionReducer = (state = {
+    answers: [ 'a', 'b', 'c', 'd'],
+    question: 'question',
+    category:'General Knowledge'
+  }, action) => {
   switch(action.type) {
     case "UPDATE_CURRENT_QUESTION":
       return { ...action.question, asked: true }
