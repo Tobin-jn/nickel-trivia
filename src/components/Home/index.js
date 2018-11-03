466f93e0b91012869a3823ed1e77c5676a34d641
Tobin Nelson <tobin.j.n@gmail.com>
2018-11-03 14:27:39 -0600

    Style Daily Question

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -13,15 +13,16 @@ export class Home extends Component {
   render() {
     return(
       <div className='landing-page'>
-      <img className='nickel-logo' src={ require('../../images/nickel.png') } alt="Nickel Jar Logo" />
-        <div className="btn-container">
-          <NavLink  to='/dailyquestion'>
-            <button className='daily-trivia-btn btn'>Trivia of the Day</button>
-          </NavLink>
-          <NavLink  to='/triviagame'>
-            <button className='trivia-game-btn btn'>Trivia Game</button>
-          </NavLink>
-        </div>
+        <h1 className="title">Welcome to <span className='trivia-text'>Nickel Trivia!</span></h1>
+        <img className='nickel-logo' src={ require('../../images/nickel.png') } alt="Nickel Jar Logo" />
+          <div className="btn-container">
+            <NavLink  to='/dailyquestion'>
+              <button className='daily-trivia-btn btn'>Trivia of the Day</button>
+            </NavLink>
+            <NavLink  to='/triviagame'>
+              <button className='trivia-game-btn btn'>Trivia Game</button>
+            </NavLink>
+          </div>
       </div>
     )
   }
