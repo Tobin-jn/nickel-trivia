3f44b223be41fd03107ad46e0f6df8e720afbc48
Tobin Nelson <tobin.j.n@gmail.com>
2018-11-03 09:56:45 -0600

    Populate daily question without having to select a category

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -14,7 +14,7 @@ export class Home extends Component {
     return(
       <div>
         <h1 className="title">Nickle Trivia</h1>
-        <NavLink className='daily-trivia-btn' to='/categories'>Trivia of the Day</NavLink>
+        <NavLink className='daily-trivia-btn' to='/dailyquestion'>Trivia of the Day</NavLink>
         <NavLink className='trivia-game-btn' to='/categories'>Trivia Game</NavLink>
       </div>
     )
--- src/containers/DailyQuestion/index.js
+++ src/containers/DailyQuestion/index.js
@@ -3,9 +3,49 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import './DailyQuestion.css';
 import Question from '../Question'
+import { updateQuestions, updateCurrentQuestion } from '../../actions'
+import { getQuestions } from '../../apiCalls/apiCalls'
+
+
 
 export class DailyQuestion extends Component {
 
+  componentDidMount() {
+    this.getTriviaQuestions()
+  }
+
+  getTriviaQuestions = async () => {
+    if(!this.checkState('generalKnowledge')) {
+      this.generateQuestion('generalKnowledge', this.props.questions['generalKnowledge'])
+      return
+    }
+
+    try {
+      const triviaQuestions = await getQuestions('generalKnowledge')
+
+      this.props.handleFetchQuestions('generalKnowledge', triviaQuestions)
+      this.generateQuestion('generalKnowledge', triviaQuestions)
+    } 
+    catch(error) {
+      console.log('we have a problem...')
+    }
+  }
+
+  checkState = (category) => {
+    if (this.props.questions[category].length === 0) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  generateQuestion = (category, questions) => {
+    const length = questions.length
+    const randomInt = Math.round(Math.random() * (length - 0));
+    const newQuestion = questions[randomInt]
+    console.log(newQuestion)
+    this.props.generateNewQuestion(newQuestion)
+  }
 
   render() {
     const { currentQuestion } = this.props
@@ -26,8 +66,8 @@ export const mapStateToProps = state => ({
 });
 
 export const mapDispatchToProps = dispatch => ({
-  // saveUserData: (username, id) => dispatch(saveUserData(username, id)),
-
+  handleFetchQuestions: (category, questions) => dispatch(updateQuestions(category, questions)),
+  generateNewQuestion: (question) => dispatch(updateCurrentQuestion(question))
 });
 
 export default connect(
