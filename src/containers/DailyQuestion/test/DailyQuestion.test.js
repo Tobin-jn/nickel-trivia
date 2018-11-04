cc46c2003941407ea64e3963ba65f0c5c1e2ad68
Tobin Nelson <tobin.j.n@gmail.com>
2018-11-04 16:50:56 -0700

    Test mapDispatchToProps on DailyQuestion.test.js

--- src/containers/DailyQuestion/test/DailyQuestion.test.js
+++ src/containers/DailyQuestion/test/DailyQuestion.test.js
@@ -3,6 +3,14 @@
 import React from "react";
 import { shallow } from "enzyme";
 import { DailyQuestion, mapStateToProps, mapDispatchToProps } from "../index";
+import { mockState, mockQuestion, mockQuestions } from './mocks';
+import { 
+  updateCategory, 
+  updateQuestions, 
+  updateCurrentQuestion, 
+  addQuestionCount,
+  toggleAsked,
+} from '../../../actions'
 
 describe("DailyQuestion", () => {
   let wrapper;
@@ -61,12 +69,47 @@ describe('mapStateToProps', () => {
   })
 
   it("should return a currentQuestion object in the props object", () => {
+    const expected = {        
+      category: "Geography",
+      question: "Which city is the capital of the United States of America?",
+      correct_answer: "Washington D.C",
+      answers: [],
+      id: 1,
+      asked: false,
+    }
 
+  const mappedProps = mapStateToProps(mockState)
+  expect(mappedProps.currentQuestion).toEqual(expected)
   })
 })
 
 describe('mapDispatchToProps', () => {
-  it('should call dispatch with...', () => {
+  const mockDispatch = jest.fn()
+  
+  it('should call dispatch with updateQuestions action when updateQuestions is called', () => {
+    const actionToDispatch = updateQuestions('geography', mockQuestions)
 
+    const mappedProps = mapDispatchToProps(mockDispatch)
+    mappedProps.updateQuestions('geography', mockQuestions)
+
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch)
+  })
+
+  it('should call dispatch with updateCurrentQuestion action when generateNewQuestion is called', () => {
+    const actionToDispatch = updateCurrentQuestion(mockQuestion)
+
+    const mappedProps = mapDispatchToProps(mockDispatch)
+    mappedProps.generateNewQuestion(mockQuestion)
+
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch)
+  })
+
+  it('should call dispatch with toggleAsked action when toggleAsked is called', () => {
+    const actionToDispatch = toggleAsked('geography', mockQuestion)
+
+    const mappedProps = mapDispatchToProps(mockDispatch)
+    mappedProps.toggleAsked('geography', mockQuestion)
+
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch)
   })
 })
