294ef5912d511b7a93807f725e222c7d8d23cdf4
Tobin Nelson <tobin.j.n@gmail.com>
2018-11-04 16:12:34 -0700

    Setup tests for Categories.js

--- src/containers/Categories/test/Categories.test.js
+++ src/containers/Categories/test/Categories.test.js
@@ -18,16 +18,76 @@ describe("Categories", () => {
   it("should match snapshot", () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  describe('handleClick', () => {
+
+  })
+
+  describe('getTriviaQuestions', () => {
+
+  })
+
+  describe('checkState', () => {
+
+  })
+
+  describe('generateQuestion', () => {
+
+  })
+
 });
 
-// describe('mapStateToProps', () => {
-//   it("should return a props object", () => {
+describe('mapStateToProps', () => {
+  it("should return a questions array in the props object", () => {
 
-//   })
-// })
+  })
 
-// describe('mapDispatchToProps', () => {
-//   it('should call dispatch with...', () => {
+  it("should return a category in the props object", () => {
 
-//   })
-// })
+  })
+})
+
+describe('mapDispatchToProps', () => {
+  it('should call dispatch with updateCategory action when handleCategory is called', () => {
+
+  })
+
+  it('should call dispatch with updateQuestions action when updateQuestions is called', () => {
+
+  })
+
+  it('should call dispatch with updateCurrentQuestion action when generateNewQuestion is called', () => {
+
+  })
+
+  it('should call dispatch with addQuestionCount action when addQuestionCount is called', () => {
+
+  })
+
+  it('should call dispatch with toggleAsked action when toggleAsked is called', () => {
+
+  })
+})
+
+
+
+
+  describe('mapStateToProps', () => {
+    it('should return an object with the todos array', () => {
+      // Setup
+      const mockState = {
+        todos: [{text: 'Learn Redux!', id: 0}],
+        filter: 'SHOW_ALL'
+      }
+      const expected = {
+        todos: [{text: 'Learn Redux!', id: 0}]
+      }
+
+      // Execution
+      const mappedProps = mapStateToProps(mockState)
+
+      // Expectation
+      expect(mappedProps).toEqual(expected)
+    })
+  })
+})
