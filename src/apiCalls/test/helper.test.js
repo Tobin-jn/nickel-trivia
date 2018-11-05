5f07ac5e97cc7fe945dcf96769e616ee30b5749b
Tobin Nelson <tobin.j.n@gmail.com>
2018-11-05 12:04:24 -0700

    Setup helper.test.js

---
+++ src/apiCalls/test/helper.test.js
@@ -0,0 +1,18 @@
+describe('shuffleAnswers', () => {
+  it('should take in an array and shuffle the order of the elements', () => {
+    
+  })
+})
+
+describe('cleanQuestions', () => {
+  it('should clean the questions to return a clean object', () => {
+
+  })
+})
+
+describe('generateQuestion', () => {
+  it('should return a single question from an array of questions', () => {
+
+  })
+  
+})
