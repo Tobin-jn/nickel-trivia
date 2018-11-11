63c045a3a5bb56e5d347f6b14dffd7663dc8662b
Tobin Nelson <tobin.j.n@gmail.com>
2018-11-10 20:15:10 -0700

    Updates

---
+++ precache-manifest.e016a5cc18f5472097d54064dc20752e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c30a14635d1e659b3e449e3fe57813a",
+    "url": "/nickel-trivia/static/media/nickel.1c30a146.png"
+  },
+  {
+    "revision": "2eebbff36fb814984a465eb75de54242",
+    "url": "/nickel-trivia/static/media/blanket.2eebbff3.jpg"
+  },
+  {
+    "revision": "46a668fdcd7e14dcff4567f3004f5e06",
+    "url": "/nickel-trivia/static/media/NickelJar.46a668fd.png"
+  },
+  {
+    "revision": "41739c880ca76ebdd9c4",
+    "url": "/nickel-trivia/static/js/runtime~main.41739c88.js"
+  },
+  {
+    "revision": "ab68531f764be1598558",
+    "url": "/nickel-trivia/static/js/main.ab68531f.chunk.js"
+  },
+  {
+    "revision": "6d82e5390ea410b777b8",
+    "url": "/nickel-trivia/static/js/1.6d82e539.chunk.js"
+  },
+  {
+    "revision": "ab68531f764be1598558",
+    "url": "/nickel-trivia/static/css/main.bdee9683.chunk.css"
+  },
+  {
+    "revision": "1e54d8189ecd31d6276a4585b6265de6",
+    "url": "/nickel-trivia/index.html"
+  }
+];
